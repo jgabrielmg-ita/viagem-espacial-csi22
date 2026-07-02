da5a4ba36987f6ea20ae430cc42280496e53b745
jgabrielmg-ita <joao.gonzaga.9076@ga.ita.br>
2026-07-02 17:18:29 -0300

    substitui constantes de config.py em background.py

--- background.py
+++ background.py
@@ -1,5 +1,6 @@
 import pygame
 
+import config
 
 class Background:
     """
@@ -11,18 +12,18 @@ class Background:
 
     def __init__(self):
 
-        background_fig = pygame.image.load("Images/background.png")
+        background_fig = pygame.image.load(config.IMG_FUNDO)
         background_fig.convert()
         self.image = background_fig
 
-        margin_left_fig = pygame.image.load("Images/margin_1.png")
+        margin_left_fig = pygame.image.load(config.IMG_MARGEM_ESQ)
         margin_left_fig.convert()
-        margin_left_fig = pygame.transform.scale(margin_left_fig, (60, 600))
+        margin_left_fig = pygame.transform.scale(margin_left_fig, (config.LARGURA_MARGEM, config.ALTURA_MARGEM))
         self.margin_left = margin_left_fig
 
         margin_right_fig = pygame.image.load("Images/margin_2.png")
         margin_right_fig.convert()
-        margin_right_fig = pygame.transform.scale(margin_right_fig, (60, 600))
+        margin_right_fig = pygame.transform.scale(margin_right_fig, (config.LARGURA_MARGEM, config.ALTURA_MARGEM))
         self.margin_right = margin_right_fig
     # __init__()
 
@@ -33,7 +34,7 @@ class Background:
     def draw(self, screen):
         screen.blit(self.image, (0, 0))
         screen.blit(self.margin_left, (0, 0))
-        screen.blit(self.margin_right, (740, 0))
+        screen.blit(self.margin_right, (config.POS_MARGEM_DIR_X, 0))
     # draw()
 
     # Define posições do Plano de Fundo para criar o movimento
@@ -41,12 +42,12 @@ class Background:
 
         #movimento background
         screen.blit(self.image, (movL_x, movL_y))
-        screen.blit(self.image, (movL_x, movL_y + 600))
+        screen.blit(self.image, (movL_x, movL_y + config.ALTURA_BLOCO_FUNDO))
         screen.blit(self.image, (movL_x, movL_y + 1200))
         screen.blit(self.image, (movL_x, movL_y + 1800))
         screen.blit(self.image, (movL_x, movL_y + 2400))
         screen.blit(self.image, (movL_x, movL_y + 3000))
-        screen.blit(self.image, (movL_x, movL_y - 600))
+        screen.blit(self.image, (movL_x, movL_y - config.ALTURA_BLOCO_FUNDO))
         screen.blit(self.image, (movL_x, movL_y - 1200))
         screen.blit(self.image, (movL_x, movL_y - 1800))
         screen.blit(self.image, (movL_x, movL_y - 2400))
@@ -57,12 +58,12 @@ class Background:
 
         # movimento margem esquerda
         screen.blit(self.margin_left, (movL_x, movL_y))
-        screen.blit(self.margin_left, (movL_x, movL_y + 600))
+        screen.blit(self.margin_left, (movL_x, movL_y + config.ALTURA_BLOCO_FUNDO))
         screen.blit(self.margin_left, (movL_x, movL_y + 1200))
         screen.blit(self.margin_left, (movL_x, movL_y + 1800))
         screen.blit(self.margin_left, (movL_x, movL_y + 2400))
         screen.blit(self.margin_left, (movL_x, movL_y + 3000))
-        screen.blit(self.margin_left, (movL_x, movL_y - 600))
+        screen.blit(self.margin_left, (movL_x, movL_y - config.ALTURA_BLOCO_FUNDO))
         screen.blit(self.margin_left, (movL_x, movL_y - 1200))
         screen.blit(self.margin_left, (movL_x, movL_y - 1800))
         screen.blit(self.margin_left, (movL_x, movL_y - 2400))
@@ -73,12 +74,12 @@ class Background:
 
         # movimento margem direita
         screen.blit(self.margin_right, (movR_x, movR_y))
-        screen.blit(self.margin_right, (movR_x, movR_y + 600))
+        screen.blit(self.margin_right, (movR_x, movR_y + config.ALTURA_BLOCO_FUNDO))
         screen.blit(self.margin_right, (movR_x, movR_y + 1200))
         screen.blit(self.margin_right, (movR_x, movR_y + 1800))
         screen.blit(self.margin_right, (movR_x, movR_y + 2400))
         screen.blit(self.margin_right, (movR_x, movR_y + 3000))
-        screen.blit(self.margin_right, (movR_x, movR_y - 600))
+        screen.blit(self.margin_right, (movR_x, movR_y - config.ALTURA_BLOCO_FUNDO))
         screen.blit(self.margin_right, (movR_x, movR_y - 1200))
         screen.blit(self.margin_right, (movR_x, movR_y - 1800))
         screen.blit(self.margin_right, (movR_x, movR_y - 2400))
